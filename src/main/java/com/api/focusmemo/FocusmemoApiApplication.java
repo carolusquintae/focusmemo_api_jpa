9d0c60d01ea8e33215d77946a0b9edbf69028891
Carlos <carolusquintae@gmail.com>
2026-03-14 18:10:15 +0100

    first commit

---
+++ src/main/java/com/api/focusmemo/FocusmemoApiApplication.java
@@ -0,0 +1,13 @@
+package com.api.focusmemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FocusmemoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FocusmemoApiApplication.class, args);
+	}
+
+}
